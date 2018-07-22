abc059521e32ab8589cd94a298dc310e36232d45
Chris Guirl <chris@guirl.net>
2018-07-22 19:32:46 -0400

    Set coindexbot version instead of BitMex MM version.

--- market_maker/__init__.py
+++ market_maker/__init__.py
@@ -4,7 +4,7 @@ import os
 import shutil
 
 
-__version__ = 'v1.3'
+__version__ = '0.0.1'
 
 
 def run():
--- market_maker/utils/constants.py
+++ market_maker/utils/constants.py
@@ -1,7 +1,7 @@
 import subprocess
 # Constants
 XBt_TO_XBT = 100000000
-VERSION = 'v1.1'
+VERSION = '0.0.1'
 try:
     VERSION = str(subprocess.check_output(["git", "describe", "--tags"], stderr=subprocess.DEVNULL).rstrip())
 except Exception as e:
